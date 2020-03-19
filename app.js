b5a9a4e5e15a367207c451d13d2365378027a4a5
Enmanuel dlN <edelanuezlang@gmail.com>
2020-03-19 10:59:33 -0400

    add home page, delete route, add route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const Book = require("./models/Book");
 const BookList = require("./models/BookList");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Setting a public folder for our static files
@@ -20,14 +20,25 @@ app.set("view engine", "ejs");
 //Create list to hold our books
 const list = new BookList();
 
+app.get('/', (req, res) => {
+  res.render('index.ejs', { books: list.books })
+})
+
 app.get("/add", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "add-book.html"));
 });
 
 app.post("/add", (req, res) => {
-  const {title, author, imageURL, description} = req.body;
+  const { title, author, imageURL, description } = req.body;
   const book = new Book(title, author, imageURL, description);
   list.addBook(book);
-  res.render("index.ejs", {name: "Steph"});
+  res.redirect("/");
 });
+
+app.post('/delete/:id', (req, res) => {
+  const bookId = req.params.id
+  list.deleteBook(bookId)
+  res.redirect("/")
+})
+
 app.listen(port, () => console.log(`Now listening on port ${port}.`));
